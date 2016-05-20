56b8f10a9783d8ff33106c7c381da751d4711934
vraid <vraid@radish>
2016-05-20 16:33:34 +0200

    simpler delisting

--- delist.c
+++ delist.c
@@ -1,5 +1,6 @@
 #include "delist.h"
 #include "data-structures.h"
+#include "global-variables.h"
 
 void check_rest_empty(object* ls) {
 	if (!is_empty_list(list_rest(ls))) {
@@ -14,16 +15,15 @@ void delist_1(object* ls, object** first) {
 
 void delist_2(object* ls, object** first, object** second) {
 	*first = list_first(ls);
-	ls = list_rest(ls);
-	*second = list_first(ls);
-	check_rest_empty(ls);
+	delist_1(list_rest(ls), second);
 }
 
 void delist_3(object* ls, object** first, object** second, object** third) {
 	*first = list_first(ls);
-	ls = list_rest(ls);
-	*second = list_first(ls);
-	ls = list_rest(ls);
-	*third = list_first(ls);
-	check_rest_empty(ls);
+	delist_2(list_rest(ls), second, third);
+}
+
+void delist_4(object* ls, object** first, object** second, object** third, object** fourth) {
+	*first = list_first(ls);
+	delist_3(list_rest(ls), second, third, fourth);
 }
