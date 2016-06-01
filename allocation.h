2a5b74432d68f55c9c9593ba0bb27e2c4ba79e81
vraid <vraid@radish>
2016-06-02 01:59:22 +0200

    unused files

--- allocation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "data-structures.h"
-
-void init_object(object_location loc, object_type type, object* obj);
-object* allocate_object(object_type type);
-
-object* new_list(void);
